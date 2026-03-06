4b971f94a2d9d1868a0e4bc6569c17e76dac78f8
MananS8805 <148432710+MananS8805@users.noreply.github.com>
2026-03-07 02:47:32 +0530

    update CORS for production

--- backend/main.py
+++ backend/main.py
@@ -86,7 +86,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://fixmyjournal.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
